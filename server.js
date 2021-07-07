6160687056a466d3ccc2dda49a92ec2241013a45
Shreekz <alanmguadet@gmail.com>
2021-07-07 14:43:25 -0400

    added routes for exercise and stats html

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,9 +16,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbnamehere", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/exercise.html'));
+});
+
+app.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/stats.html'));
+});
 
 app.listen(PORT, () => {
   //change log before pushing to server
-  console.log(`App running at http:localhost:${PORT}`)
+  console.log(`App running at http://localhost:${PORT}`)
 })
