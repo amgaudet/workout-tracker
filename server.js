4e77011bf733c9e5aafe65b021480284e699b4a6
Shreekz <alanmguadet@gmail.com>
2021-07-11 18:45:16 -0400

    Deploying to heroku

--- server.js
+++ server.js
@@ -15,7 +15,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 //get all work outs for index.html
 app.get('/api/workouts', (req, res) => {
