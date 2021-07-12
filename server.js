a6ed31435e43a951ed6e42cb021fb1f4c6ad1112
Shreekz <alanmguadet@gmail.com>
2021-07-12 15:28:25 -0400

    fixed db bug for staging to heroku

--- server.js
+++ server.js
@@ -15,15 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 //get all work outs for index.html
 app.get('/api/workouts', (req, res) => {
