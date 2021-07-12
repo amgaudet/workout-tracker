0c4790f46b393028c79a25528759fbab8578d803
Shreekz <alanmguadet@gmail.com>
2021-07-12 16:34:18 -0400

    db bug

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
 
 //get all work outs for index.html
 app.get('/api/workouts', (req, res) => {
